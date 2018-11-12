63f5292c195515f101e33f0184393ad0d94bd924
Tekapo <yuns0316@gmail.com>
2018-11-12 18:26:47 +0900

    2
    2

---
+++ components/container.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    initActive();
+    initBar();
+    sliderFunc();
+});
+
+$(window).resize(function(){
+  initBar();
+});
+
+function initBar() {
+    var barWidth = $(".active").css('width');
+    var barOffset = $(".active").offset().left;
+    $(".bar").css('width', barWidth);
+    $(".bar").css('left', barOffset);
+};
+
+function initActive() {
+    $(".portfolio").addClass("active");
+    $("#section").load("./components/portfolio/portfolio.html");
+};
+
+
+function sliderFunc() {
+    $(".portfolio").click(function(){
+        var a = $(this).hasClass("active");
+        if (a == false) {
+            $("#section").load("./components/portfolio/portfolio.html");
+        }
+    });
+    $(".education").click(function(){
+        var a = $(this).hasClass("active");
+        if (a == false) {
+        $("#section").load("./components/education/education.html");
+        }
+    })
+    $(".info").click(function(){
+        var a = $(this).hasClass("active");
+        if (a == false) {
+        $("#section").load("./components/info/info.html");
+        }
+    })
+    $(".slide-item").click(function(){
+        var a = $(".slide-item").hasClass("active");
+         if( a ){
+             $(".slide-item").removeClass("active");
+             $(this).addClass("active");
+         }
+         initBar();
+    });
+}
