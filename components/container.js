10c09ada145a371b292b5931f5d51f8275143769
Tekapo <yuns0316@gmail.com>
2018-11-15 00:15:11 +0900

    9
    9

--- components/container.js
+++ components/container.js
@@ -38,7 +38,7 @@ function sliderFunc() {
         var a = $(this).hasClass("active");
         if (a == false) {
         $("#section").load("./components/info/info.html");
-        $("#skills").addClass("active");
+        $(".skills").addClass("active");
         }
     });
     $(".slide-item").click(function(){
